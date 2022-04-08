5653e109376bf3bcd2c96f0b0865dd0e75d67110
muxiaochen <631032057@qq.com>
2022-04-08 19:36:27 +0800

    private

--- src/class.ts
+++ src/class.ts
@@ -1,10 +1,15 @@
 // 类成员的属性都是实例属性，方法都是实例方法
 class Dog{
+    // private私有成员熟悉，这个类既不能实例化，也不能被继承
     constructor(name: string){
         this.name = name
     }
     name: string
     run() {}
+    // 私有成员 只能类的本身调用
+    private pri(){}
+
+    // 受保护成员
 }
 
 // name属性只在实例上不在原型上，实例的属性必须具有初始值，或者在构造函数中被初始化
@@ -13,3 +18,10 @@ let dog = new Dog('wangwang')
 console.log(dog) 
 
 // 类的继承
+class Husky extends Dog{
+    constructor(name:string, color:sting){
+        super(name)
+        this.color = color;
+    }
+    color: string
+}
