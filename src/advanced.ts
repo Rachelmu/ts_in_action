eee0d96b91f4caa55cf0fe3df2ab232a62cd3a12
muxiaochen <631032057@qq.com>
2022-04-20 20:08:18 +0800

    类型推断

--- src/advanced.ts
+++ src/advanced.ts
@@ -75,3 +75,67 @@ interface Point2D {
 }
 let p3d = (point: Point3D) => {};
 let p2d = (point: Point2D) => {};
+p3d = p2d
+// p2d = p3d
+
+// 3）返回值类型
+let advF = () => ({name: 'Alice'});
+let advG = () => ({name: 'Alice', location: 'Beijing'});
+
+// 函数重载
+function overload(a:number, b:number): number;
+function overload(a:string, b:string): string;
+function overload(a:any, b:any): any{}
+
+// 枚举兼容性
+enum Fruit{ Apple, Banana }
+enum Color{ Red, Yellow }
+
+let fruit: Fruit.Apple = 3
+let no: number = Fruit.Apple
+// 枚举之间完全不兼容
+// let color: Color.Red = Fruit.Apple
+
+// 类兼容性
+// 静态成员与构造函数不参与比较
+// 私有成员只有父类跟子类是相互兼容的
+class A {
+    constructor(p:number, q:number){}
+    id:number = 1
+    // private name:string = ''
+}
+class B{
+    static s =1
+    constructor(p: number){}
+    id: number = 2
+}
+let aa = new A(1,2);
+let bb = new B(1);
+
+class C extends A{}
+// let cc = new C(1,2)
+
+
+// 泛型兼容性
+// 类型成员T在泛型中使用的时候 才会影响泛型兼容性
+interface Empty<T>{
+    value: T
+}
+// let obj1 : Empty<number> = {};
+// let obj2 : Empty<string> = {};
+// obj1 = obj2
+
+let advlog1 = <T>(x: T): T => {
+    console.log('x')
+    return x
+}
+
+let advlog2 = <U>(y: U): U => {
+    console.log('y')
+    return y
+}
+advlog1 = advlog2
+
+// 口诀
+// 结构之间兼容：成员少的兼容成员多的
+// 函数之间兼容：参数多的兼容参数少的
